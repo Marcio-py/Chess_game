9ea00c898523db60b2caf23d5fe49870d0cc758b
Marcio <marcio1961.ma@gmail.com>
2023-07-29 14:05:17 +0100

    implementar funcionalidade de xeque

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -1,24 +1,32 @@
-﻿namespace tabuleiro{
-	abstract class Peca{
+﻿namespace tabuleiro
+{
+    abstract class Peca
+    {
 
-		public Posicao posicao { get; set; }
-		public Cor cor { get; protected set; }
-		public int qteMovimentos { get; protected set; }
-		public Tabuleiro tab { get; protected set; }
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
 
-		public Peca( Tabuleiro tab, Cor cor)
-		{
-			this.posicao = null;
-			this.tab = tab;
-			this.cor = cor;
-			this.qteMovimentos = 0;
+        public Peca(Tabuleiro tab, Cor cor)
+        {
+            this.posicao = null;
+            this.tab = tab;
+            this.cor = cor;
+            this.qteMovimentos = 0;
 
-		}
+        }
 
-		public void incrementarMovimento()
-		{
-			qteMovimentos++;
-		}
+        public void incrementarMovimento()
+        {
+            qteMovimentos++;
+        }
+
+
+        public void decrementarQteMovimentos()
+        {
+            qteMovimentos--;
+        }
 
         public bool existeMovimentosPossiveis()
         {
